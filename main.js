aa3c0bdf64b458c03dc028071f2980f72be275d4
Asif6218 <mohdasif6227@gmail.com>
2021-08-14 23:16:42 -0600

    dom1

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ console.log(document.title);
 //2.element by tag name
 //3.element by class name
 //4.query selector 
-//5.query selecto all
+//5.query selector all
 
 //element by id
 
@@ -33,3 +33,8 @@ console.log(heading1);
 
 let des3=document.querySelector("h1");
 console.log(des3);
+
+//query selector all
+
+let des4=document.querySelectorAll(".second");
+console.log(des4[0]);
