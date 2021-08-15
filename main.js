b33eccf1e1a6b37babca3770e5c8d6421e0fc268
Asif6218 <mohdasif6227@gmail.com>
2021-08-14 23:05:14 -0600

    dom

---
+++ main.js
@@ -0,0 +1,35 @@
+//changing title
+
+console.log(document.title);
+document.title ="modified";
+console.log(document.title);
+
+//DOM selectors
+//1.element by id
+//2.element by tag name
+//3.element by class name
+//4.query selector 
+//5.query selecto all
+
+//element by id
+
+let  des=document.getElementById("first");
+console.log(des);
+
+//element by tag name
+
+let heading =document.getElementsByTagName("h1");
+console.log(heading[0]);
+
+//element by class name
+
+let des2=document.getElementsByClassName("second");
+console.log(des2[0]);
+
+//query selector
+
+let heading1=document.querySelector("#first");
+console.log(heading1);
+
+let des3=document.querySelector("h1");
+console.log(des3);
